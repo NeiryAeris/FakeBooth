a522cf91315e2b7fa5bd76906889da4d2b268aa0
NeiryAeris <Kukorysenpai3506@gmail.com>
2024-04-29 22:32:32 +0700

    adding smt

---
+++ neirylittlebox/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '/Navbar.css'
+import icon from '..Assets/icon.jpg'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="nav-logo">
+                <img src="" alt="" />
+            </div>
+        </div>
+    )
+}
