7a3d36b5c88285ac2465c40abf1648125d9e4bf3
NeiryAeris <Kukorysenpai3506@gmail.com>
2024-05-03 12:48:06 +0700

    ki

--- neirylittlebox/src/Components/Navbar/Navbar.jsx
+++ neirylittlebox/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import './Navbar.css';
 import icon from '../Assets/icon.jpg';
+import cart_icon from '../Assets/cart_icon.png'
 
 const Navbar = () => {
+
+    const [menu,setMenu] = useState("Gallery")
+    
     return (
         <div className="navbar">
             <div className="nav-logo">
-                <img src="icon" alt="" />
-                some line to check
+                <img src={icon} alt="" />
+                <p>Little Box</p>
+            </div>
+            <ul className="nav-menu">
+                <li onClick={() => {setMenu('Gallery')}}>Gallery{menu === "Gallery"?<hr/>:<></>}</li>
+                <li onClick={() => {setMenu('Booth')}}>Booth{menu === "Booth"?<hr/>:<></>}</li>
+                <li onClick={() => {setMenu('Categories')}}>Categories{menu === "Categories"?<hr/>:<></>}</li>
+            </ul>
+            <div className="nav-login-cart">
+                <button>Login</button>
+                <img src={cart_icon} alt="" />
+                <div className="nav-cart-count">0</div>
             </div>
         </div>
     )
---
+++ neirylittlebox/src/pages/BoxCategory.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BoxCategory = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BoxCategory
