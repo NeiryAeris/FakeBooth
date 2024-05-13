a6b7fc5d634f8973fe137eb0721ce69d70b282fb
NeiryAeris <Kukorysenpai3506@gmail.com>
2024-05-13 23:32:38 +0700

    adjustment

--- backend/index.js
+++ backend/index.js
@@ -87,17 +87,55 @@ const Product = mongoose.model("Product", {
   },
   available:{
     type:Boolean,
-    required:true,
+    default:true,
   }
 });
 
-app.post('/addproduct',async (req,res) =>{
-    const product = new Product({
-        id:req.body.id,
-        name:req.body.name,
-        image:req.body.image,
-        category:req.body.category,
-        new_price:req.body.new_price,
-        old_price:req.body.old_price,
-    })
+const Users= mongoose.model('Users',{
+  name:{
+      type: String,
+  },
+  email:{
+      type: String,
+      unique:true,
+  },
+  password:{
+      type: String,
+  },
+  cartData:{
+      type: Object,
+  },
+  date:{
+      type: Date,
+      default:Date.now,
+  }
+})
+
+
+app.post('/addproduct',async(req,res)=>{
+  let products = await Product.find({});
+  let id;
+  if(products.length>0){
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id+1
+  }
+  else{
+    id = 1;
+  }
+  const product = new Product({
+      id:id,
+      name:req.body.name,
+      image:req.body.image,
+      category:req.body.category,
+      new_price:req.body.new_price,
+      old_price:req.body.old_price,
+  });
+  console.log(product);
+  await product.save();
+  console.log("Saved");
+  res.json({
+      success:true,
+      name:req.body.name,
+  })
 })
