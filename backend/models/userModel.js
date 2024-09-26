11c9897e7623de787a1ac1163d9d362d04d4bede
NeiryAeris <Kukorysenpai3506@gmail.com>
2024-09-26 21:13:33 +0700

    changes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,10 +4,9 @@ const userSchema = new mongoose.Schema({
   username: { type: String, require: true },
   email: { type: String, unique: true },
   password: { type: String, require: true },
-  cartData: { type: Object },
-  date: { type: Date, default: Date.now },
+  cartData: { type: Object, default: {} },
 });
 
 const userModel = mongoose.models.users || mongoose.model("users", userSchema);
 
-export default userModel
+export default userModel;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from "express";
-import dbConnect from './dbContext.js'
+import dbConnect from "./dbContext.js";
 import jwt from "jsonwebtoken";
 import multer from "multer";
 import path from "path";
 import cors from "cors";
 import { equal } from "assert";
-import productRouter from './routes/productRoutes.js'
+import productRouter from "./routes/productRoutes.js";
+import cartRouter from "./routes/cartRoutes.js";
 
 const port = 4000;
 const app = express();
@@ -13,11 +14,12 @@ app.use(express.json());
 app.use(cors());
 
 dbConnect();
- 
 
-app.use('/products',productRouter);
-app.get('/', (req, res) => {
-    res.send('Express App is running')
+app.use("/products", productRouter);
+app.use("/cart", cartRouter);
+
+app.get("/", (req, res) => {
+  res.send("Express App is running");
 });
 
-app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
+app.listen(port, () => console.log(`Server started on http://localhost:${port}`));
